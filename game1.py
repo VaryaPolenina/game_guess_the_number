047997cddec5b8aa4f58c3745c7f05d27a51c979
VaryaPolenina <167534895+VaryaPolenina@users.noreply.github.com>
2026-01-18 18:33:52 +0300

    Update game1.py

--- game1.py
+++ game1.py
@@ -14,36 +14,6 @@ def guess_the_number():
         except ValueError:
             print("Пожалуйста, введи целое число.")
             continue
-        popitka += 1
-        if predpolozhenie < secretik:
-            print("Твое число меньше загаданного.")
-        elif secretik < predpolozhenie:
-            print("Твое число больше загаданного.")
-        else:
-            print(f"Поздравляю, ты угадал число. Загаданное число было: {secretik}.")
-            return
-
-            print(f"К сожалению ты использовал все попытки. Загаданное число было: {secretik}. Удачи в следующий раз!")
-            break
-    else:
-        print(f"")
-guess_the_number()
-import random
-
-def guess_the_number():
-    secretik = random.randint(1, 100)
-    popitka = 0
-    max_popitka = 10
-
-    print("Привет, добро пожаловать в игру 'Угадай число'!")
-    print(f"Я уже загадал число от 1 до 100. У тебя {max_popitka} попыток.")
-
-    while popitka < max_popitka:
-        try:
-            predpolozhenie = int(input(f"Твоя {popitka + 1} попытка. Твое предположение: "))
-        except ValueError:
-            print("Пожалуйста, введи целое число.")
-            continue
 
         popitka += 1
 
@@ -60,5 +30,6 @@ def guess_the_number():
     print("\nДля повторной игры нажми на любую клавишу.")
     input()
     guess_the_number()
+
 
 guess_the_number()
